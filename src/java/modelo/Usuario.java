4bd7eb45940c05de82fb99d56b367aed151d142f
yeifer alcala <yeiferalcala23@gmail.com>
2024-11-17 04:28:04 -0500

    creamos el codigo para la clase usuario

--- src/java/modelo/Usuario.java
+++ src/java/modelo/Usuario.java
@@ -3,12 +3,106 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
+import java.sql.Date;
 
 /**
  *
  * @author JEIFER ALCALA
  */
-public class Usuario {
-    
+
+    public class Usuario {
+    private int id;  // El id es autoincremental
+    private String password;
+    private String nombre;
+    private String apellidos;
+    private String rol;
+    private String email;
+    private String telefono;
+    private String estado;
+    private Date fechaRegistro;  // Para almacenar la fecha de registro
+
+    // Constructor
+    public Usuario(String password, String nombre, String apellidos, String rol, String email) {
+        this.password = password;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.rol = rol;
+        this.email = email;
+    }
+
+    // Métodos getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
 }
+
+
     
