128cbbf37810d3cc37639df8bf89464c9963a90f
yeifer alcala <yeiferalcala23@gmail.com>
2024-11-19 02:09:31 -0500

    correccion de errores

--- src/java/controladores/ServletUsuario.java
+++ src/java/controladores/ServletUsuario.java
@@ -34,9 +34,10 @@ public class ServletUsuario extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-       try {
-    String accion = request.getParameter("accion"); // Capturar la acción
+        String accion = request.getParameter("accion"); // Capturar la acción
     String redireccion = null;
+        try {
+   
    if (accion != null ){
     if (accion.equals("agregar")) {
         CRUDusuario crudAlguien = new CRUDusuario();
